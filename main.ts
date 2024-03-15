8091adf869efeda01ab8462487f576b817ad220b
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-15 22:18:32 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+// //Question no 40
+// function make_album(artist:string, title: string , tracks?: number) {
+//     let album = {artist,title};
+//     if (tracks) {
+//         album['tracks'] = tracks;
+//     }
+//       return album;
+// }
+// console.log(make_album("Artist One","The First Album"));
+// console.log(make_album("Artist Two","The Second Album"));
+// console.log(make_album("Artist Three","The Third Album",12));
+
+// //question no 41
+// let magicians: string[] = ["David","Royal","Naqash"];
+
+// function show_magician(magicians:string[]){
+//     magicians.forEach(magician =>{
+//         console.log(magician);
+//     });
+
+// }
+// show_magician(magicians);
+
+
+// //Question no 42
+// function make_great(magicians:string[]) {
+//     for (let i = 0;  i  < magicians.length; i++){
+//     magicians[i]  + " the great";
+
+//     }
+// }
+// make_great (magicians);
+// show_magician(magicians);
+   
